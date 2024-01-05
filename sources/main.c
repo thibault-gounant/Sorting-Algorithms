25f2c05e33f81124c5649b90a31497248a86bcaa
Thibault GOUNANT <thibault.gounant@etu.univ-nantes.fr>
2024-01-05 00:00:00 +0000

    Add SDL2

--- sources/main.c
+++ sources/main.c
@@ -1,6 +1,51 @@
+#define SDL_MAIN_HANDLED
+#include <SDL2/SDL.h>
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define WIDTH 800
+#define HEIGHT 600
+
 
 int main(int argc, char* argv[]) {
-    printf("Hello, World!\n");
-    return 0;
+
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        fprintf(stderr, "SDL init error: %s\n", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("Sorting Algorithms", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
+    if (!window) {
+        fprintf(stderr, "Window creation error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        fprintf(stderr, "Renderer creation error: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
+
+    SDL_Event event;
+    bool running = true;
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT)
+                running = false;
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return EXIT_SUCCESS;
 }
